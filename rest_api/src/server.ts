cc50bb992dec35a67c58a40db8bad34e7086e59c
Arón Díaz <arondiaz.cpn@gmail.com>
2025-03-27 10:23:56 -0300

    added sequelize orm and connected to database

--- rest_api/src/server.ts
+++ rest_api/src/server.ts
@@ -1,5 +1,19 @@
 import express from "express";
 import router from "./router";
+import db from "./config/db";
+
+//connect db
+
+async function connectDB() {
+  try {
+    await db.authenticate();
+    db.sync();
+    console.log("connection successful");
+  } catch (error) {
+    console.log(error);
+  }
+}
+connectDB();
 
 const server = express();
 
