d02c65a14d165eb2e5d2e4e9d4a163b1758141c9
Arón Díaz <arondiaz.cpn@gmail.com>
2025-03-27 11:09:04 -0300

    added colors on console message

--- rest_api/src/index.ts
+++ rest_api/src/index.ts
@@ -1,6 +1,10 @@
 import server from "./server";
+import colors from "colors";
+import dotenv from "dotenv";
 
+dotenv.config()
+const port = process.env.PORT || 4000;
 
-server.listen(4000, () =>{
-    console.log(`Rest API puerto 4000`);
-})
+server.listen(port, () => {
+  console.log(colors.cyan(`Rest API working on port ${port}`));
+});
--- rest_api/src/server.ts
+++ rest_api/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import router from "./router";
 import db from "./config/db";
+import colors from "colors";
 
 //connect db
 
@@ -8,9 +9,10 @@ async function connectDB() {
   try {
     await db.authenticate();
     db.sync();
-    console.log("connection successful");
+    console.log(colors.bold.green("connection successful"));
   } catch (error) {
     console.log(error);
+    console.log(colors.red("connection refused"));
   }
 }
 connectDB();
