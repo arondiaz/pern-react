4129342c99193ccfce940cc765e0e4964b556e70
Arón Díaz <arondiaz.cpn@gmail.com>
2025-04-08 18:13:48 -0300

    added documentation of get all products with swagger

--- rest_api/src/config/swagger.ts
+++ rest_api/src/config/swagger.ts
@@ -3,11 +3,16 @@ import swaggerJSDoc from "swagger-jsdoc";
 const options: swaggerJSDoc.Options = {
   swaggerDefinition: {
     openapi: "3.0.2",
+    //agrupacion de endpoints, en caso de necesitar otro endpoint, como para usuarios por ejemplo.. creamos otro objeto dentro del tag
     tags: [
       {
         name: "Products",
         description: "API operations related to products",
       },
+      // {
+      //   name: "Usuarios",
+      //   description: "API operations related to products",
+      // },
     ],
     info: {
       title: "Rest API Node.js / Express / TypeScript",
--- rest_api/src/router.ts
+++ rest_api/src/router.ts
@@ -12,6 +12,55 @@ import { handleInputErrors } from "./middleware";
 
 const router = Router();
 
+/**
+ * @swagger
+ * components:
+ *    schemas:
+ *      Product:
+ *          type: object
+ *          properties:
+ *              id:
+ *                  type: integer
+ *                  description: Product ID
+ *                  example: 1
+ *              name:
+ *                  type: string
+ *                  description: Product name
+ *                  example: Auriculares DT770 PRO
+ *              price:
+ *                  type: number
+ *                  description: Product price
+ *                  example: 2000
+ *              availability:
+ *                  type: boolean
+ *                  description: The product availability
+ *                  example: true
+ *                   
+ *                   
+ * 
+ */
+
+/**
+ * @swagger
+ * /api/products:
+ * 
+ *      get:
+ *          summary: Get a list of products
+ *          tags: 
+ *              - Products
+ *          description: Return a list of product
+ *          responses: 
+ *             200:
+ *                 description: Successful response
+ *                 content: 
+ *                     application/json:
+ *                         schema:
+ *                                type: array
+ *                                items:
+ *                                  $ref: '#/components/schemas/Product'
+ * 
+ */
+
 router.get("/", getProducts);
 
 router.get(
