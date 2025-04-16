f27ac297c24f29489f1e34fe6a5232571fe1d488
Arón Díaz <arondiaz.cpn@gmail.com>
2025-04-16 08:45:26 -0300

    implemented form component

--- client/src/views/EditProduct.tsx
+++ client/src/views/EditProduct.tsx
@@ -2,6 +2,7 @@ import { Link, Form, LoaderFunctionArgs, redirect, useLoaderData, useActionData,
 import { getProductById, updateProduct } from "../services/productServices";
 import { Product } from "../types";
 import ErrorMessage from "../components/ErrorMessage";
+import ProductForm from "../components/ProductForm";
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   if (params.id !== undefined) {
@@ -56,32 +57,9 @@ const EditProduct = () => {
       </div>
 
       <Form className="mt-10" method="POST">
-        <div className="mb-4">
-          <label className="text-gray-800" htmlFor="name">
-            Nombre Producto:
-          </label>
-          <input
-            id="name"
-            type="text"
-            className="mt-2 block w-full p-3 bg-gray-50"
-            placeholder="Nombre del Producto"
-            name="name"
-            defaultValue={product.name}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="text-gray-800" htmlFor="price">
-            Precio:
-          </label>
-          <input
-            id="price"
-            type="number"
-            className="mt-2 block w-full p-3 bg-gray-50"
-            placeholder="Precio Producto. ej. 200, 300"
-            name="price"
-            defaultValue={product.price}
-          />
-        </div>
+        <ProductForm
+        product={product}
+        />
 
         <div className="mb-4">
           <label className="text-gray-800" htmlFor="availability">
--- client/src/views/NewProduct.tsx
+++ client/src/views/NewProduct.tsx
@@ -1,6 +1,13 @@
-import { Link,Form, useActionData, ActionFunctionArgs, redirect } from "react-router-dom";
+import {
+  Link,
+  Form,
+  useActionData,
+  ActionFunctionArgs,
+  redirect,
+} from "react-router-dom";
 import ErrorMessage from "../components/ErrorMessage";
 import { productServices } from "../services/productServices";
+import ProductForm from "../components/ProductForm";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const data = Object.fromEntries(await request.formData());
@@ -10,7 +17,7 @@ export const action = async ({ request }: ActionFunctionArgs) => {
     error = "Todos los campos son obligatorios";
   }
 
-  await productServices(data)
+  await productServices(data);
 
   return error.length > 1 ? error : redirect("/");
 };
@@ -32,30 +39,8 @@ const NewProduct = () => {
       </div>
 
       <Form className="mt-10" method="POST">
-        <div className="mb-4">
-          <label className="text-gray-800" htmlFor="name">
-            Nombre Producto:
-          </label>
-          <input
-            id="name"
-            type="text"
-            className="mt-2 block w-full p-3 bg-gray-50"
-            placeholder="Nombre del Producto"
-            name="name"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="text-gray-800" htmlFor="price">
-            Precio:
-          </label>
-          <input
-            id="price"
-            type="number"
-            className="mt-2 block w-full p-3 bg-gray-50"
-            placeholder="Precio Producto. ej. 200, 300"
-            name="price"
-          />
-        </div>
+        <ProductForm />
+
         <input
           type="submit"
           className="mt-5 w-full bg-purple-800 p-2 text-white font-bold text-lg cursor-pointer rounded"
